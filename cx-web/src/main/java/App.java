c8ac07ea3b9d987d35b9b47041cb63e3358c29d0
lu.shaofeng <lu.shaofeng@kjt.com>
2018-04-27 22:27:16 +0800

    去除无效的代码

--- cx-web/src/main/java/App.java
+++ cx-web/src/main/java/App.java
@@ -11,11 +11,7 @@ public class App {
         context.start();
         ICustomerService customerService = (ICustomerService) context.getBean("customerService");
 
-        CustomerDto customerDto = new CustomerDto();
-        customerDto.setName("Shawn");
-        customerDto.setAge(Integer.valueOf(30));
-        customerService.sayHello(customerDto);
-        System.out.println(customerService.selfIntroduce(customerDto));
+
         try {
             System.in.read();
         } catch (IOException e) {
