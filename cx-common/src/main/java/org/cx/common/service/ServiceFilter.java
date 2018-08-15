7cc295363aef1959ed38aacbc05cad371efdfb34
lushaofeng <shawn.lu@live.com>
2018-08-15 22:36:52 +0800

    dubbo service filter

--- cx-common/src/main/java/org/cx/common/service/ServiceFilter.java
+++ cx-common/src/main/java/org/cx/common/service/ServiceFilter.java
@@ -4,14 +4,26 @@ import com.alibaba.dubbo.rpc.Filter;
 import com.alibaba.dubbo.rpc.Invocation;
 import com.alibaba.dubbo.rpc.Invoker;
 import com.alibaba.dubbo.rpc.Result;
+import com.alibaba.dubbo.rpc.RpcContext;
 import com.alibaba.dubbo.rpc.RpcException;
+import org.cx.common.util.RequestID;
 
 public class ServiceFilter implements Filter{
 
+	private String reqidKey = "ReqId";
+	
 	@Override
 	public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
-		// TODO Auto-generated method stub
-		return null;
+		RpcContext context = RpcContext.getContext();
+		boolean provider = context.isProviderSide();
+		if (provider) {
+			String reqId = invocation.getAttachment(reqidKey);
+			RequestID.set(reqId);
+		} else {
+			String reqId = RequestID.get();
+			invocation.getAttachments().put(reqidKey, reqId);
+		}
+		return invoker.invoke(invocation);
 	}
 
 }
